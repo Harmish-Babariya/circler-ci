3fbdbae9ff6440885afcff8a0fbc9d64583b96f2
harmish <harmishbabariya@gmail.com>
2023-06-06 12:23:56 +0530

    Added Prod function and test case

--- math.js
+++ math.js
@@ -1,5 +1,12 @@
 function sum(a,b) {
-    return a + b + 12
+    return a + b
 }
 
-exports.sum = sum
+function prod(a,b) {
+    return a * b
+}
+
+module.exports = {
+    sum: sum,
+    prod: prod
+}
